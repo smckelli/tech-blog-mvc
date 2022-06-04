16c4a9e1e0a955cff2c8880af3d76397c0e08746
smckelli <smckelli@gmail.com>
2022-06-04 11:56:53 -0500

    tweaked the server.js file from starter

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
+const helpers = require('./utils/helpers');
+const hbs = exphbs.create({ helpers });
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -21,15 +23,11 @@ const sess = {
 
 app.use(session(sess));
 
-const helpers = require('./utils/helpers');
-
-const hbs = exphbs.create({ helpers });
-
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(require('./controllers/'));
